9ea4024904f5ed0b2b0317174b0007f91e8cc33f
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-17 15:47:48 -0800

    Update settings.py

--- django_homefinder/settings.py
+++ django_homefinder/settings.py
@@ -11,10 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "&w0a#i_*ml86%)vf5)z*-6&^dilvaytju(6lce6p0y1cq299$*"
+# SECRET_KEY = "&w0a#i_*ml86%)vf5)z*-6&^dilvaytju(6lce6p0y1cq299$*"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "&w0a#i_*ml86%)vf5)z*-6&^dilvaytju(6lce6p0y1cq299$*"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
 
 ALLOWED_HOSTS = []
@@ -71,17 +75,6 @@ WSGI_APPLICATION = "django_homefinder.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'homefinder',
-#         'USER': 'xinyuwen',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 DATABASES = {
     "default": {
